a530156506ced8d8ad0fce7b1f0f1990d7c99c5e
prabu <prabus6@outlook.com>
2024-03-17 19:50:20 +0530

    fix commit issue

--- terraform-code/var.tf
+++ terraform-code/var.tf
@@ -3,11 +3,19 @@ variable "region" {
 }
 
 variable "vpc_id" {
+<<<<<<< HEAD
+  default = "vpc-054211ba9b2e44326"
+}
+
+variable "public_subnet_ids" {
+  default = "subnet-07aa14d7eefb07bbf"
+=======
   default = "vpc-00c809216d6f1f614"
 }
 
 variable "public_subnet_ids" {
   default = "subnet-0adec26d1c731bb09"
+>>>>>>> 4ed3a9986e139d875ad9dc4b793dc7f7046e77d9
 }
 
 variable "ami_id" {
